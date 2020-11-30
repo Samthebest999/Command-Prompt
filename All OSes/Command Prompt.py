fe01bb0f54081b8d9d074e6706211b2b634cfe17
Samthebest999 <samit.mohnot@gmail.com>
2020-11-30 10:55:48 -0600

    Update Command Prompt.py

--- All OSes/Command Prompt.py
+++ All OSes/Command Prompt.py
@@ -1,1 +1,13 @@
-
+import os
+uselessnum = 0
+name=input("Please Enter Your Username:")
+print("Ok, Hi " + name)
+while uselessnum== 0:
+    cmd=input("$" + name + ":")
+    if cmd == "stop":
+        break
+        quit()
+    elif cmd == "Stop":
+        break
+        quit()
+    os.system(cmd)
